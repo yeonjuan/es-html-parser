4d6d82fabc709ec2f869101bf30a30b1b7ec4951
yeonjuan <yeonjuan93@naver.com>
2022-09-08 12:46:55 +0900

    Remove NodeTypes.AttributeAssignment

--- src/constants/node-types.ts
+++ src/constants/node-types.ts
@@ -12,7 +12,6 @@ export enum NodeTypes {
   Attribute = "Attribute",
   AttributeKey = "AttributeKey",
   AttributeValue = "AttributeValue",
-  AttributeAssignment = "AttributeAssignment",
   AttributeValueWrapperStart = "AttributeValueWrapperStart",
   AttributeValueWrapperEnd = "AttributeValueWrapperEnd",
   CloseTag = "CloseTag",

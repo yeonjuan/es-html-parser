2ace5b41fe42abe273b46cf24558a2d99ed619ec
YeonJuan <yeonjuan93@naver.com>
2024-11-27 23:26:43 +0900

    Update token-adapter.ts

--- src/types/token-adapter.ts
+++ src/types/token-adapter.ts
@@ -1,15 +1,6 @@
 import type { SourceLocation } from "./source-location";
 import type { Range } from "./range";
 import type { AnyToken } from "./token";
-import { TokenTypes } from "../constants";
-
-export type TemplateSyntaxInfo = {
-  type:
-    | TokenTypes.TemplateSyntaxStart
-    | TokenTypes.TemplateSyntaxContent
-    | TokenTypes.TemplateSyntaxEnd;
-  range: Range;
-};
 
 export interface TokenAdapter {
   finalizeRange(token: Omit<AnyToken, "loc">): Range;

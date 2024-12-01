d64835c7584b06b1ac39eabf08b47abfd53e59a9
YeonJuan <yeonjuan93@naver.com>
2024-12-01 21:53:53 +0900

    fix: apply token adapter to template tokens

--- src/utils/create-templates.ts
+++ src/utils/create-templates.ts
@@ -1,17 +1,25 @@
 import { TokenTypes } from "../constants";
-import { TemplatableToken, TokenizerState } from "../types";
+import { TemplatableToken, TokenAdapter, TokenizerState } from "../types";
 
 export function createTemplates<T extends TokenTypes>(
   state: TokenizerState,
-  type: T
+  type: T,
+  tokenAdapter: TokenAdapter
 ): TemplatableToken<T>[] {
   return state.mode === "template" && state.accumulatedContent.hasTemplate()
-    ? state.accumulatedContent.getTemplates().map((chars) => ({
-        type: type,
-        range: chars.range,
-        loc: state.sourceCode.getLocationOf(chars.range),
-        isTemplate: chars.isTemplate,
-        value: chars.value,
-      }))
+    ? state.accumulatedContent.getTemplates().map((chars) => {
+        const token = {
+          type: type,
+          range: chars.range,
+          loc: state.sourceCode.getLocationOf(chars.range),
+          isTemplate: chars.isTemplate,
+          value: chars.value,
+        };
+        return {
+          ...token,
+          range: tokenAdapter.finalizeRange(token),
+          loc: tokenAdapter.finalizeLocation(token),
+        };
+      })
     : [];
 }

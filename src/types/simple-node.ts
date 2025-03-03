6716718c038ba679b3a09e2873413f6c8b50d34f
YeonJuan <yeonjuan93@naver.com>
2025-03-03 21:29:46 +0900

    fix: incorrect types (#30)
    * fix: incorrect types
    
    * fix format

--- src/types/simple-node.ts
+++ src/types/simple-node.ts
@@ -6,20 +6,25 @@ export interface SimpleNode<T extends NodeTypes> extends BaseNode {
   value: string;
 }
 
-export interface PartNode<T extends NodeTypes> extends BaseNode {
+export interface PartNodeBase<T extends Exclude<NodeTypes, NodeTypes.Part>>
+  extends BaseNode {
   type: NodeTypes.Part;
   partOf: T;
   value: string;
 }
 
-export interface TemplateNode<T extends NodeTypes> extends BaseNode {
+export interface TemplateNodeBase<
+  T extends Exclude<NodeTypes, NodeTypes.Template | NodeTypes.Part>
+> extends BaseNode {
   type: NodeTypes.Template;
   partOf: T;
   open?: SimpleNode<NodeTypes.OpenTemplate>;
   close?: SimpleNode<NodeTypes.CloseTemplate>;
 }
 
-export interface CompositeNode<T extends NodeTypes> extends BaseNode {
-  parts: (PartNode<T> | TemplateNode<T>)[];
+export interface CompositeNode<
+  T extends Exclude<NodeTypes, NodeTypes.Template | NodeTypes.Part>
+> extends BaseNode {
+  parts: (PartNodeBase<T> | TemplateNodeBase<T>)[];
   value: string;
 }

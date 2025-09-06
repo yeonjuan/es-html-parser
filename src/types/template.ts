7edeaf6c422ed1de46c8f4350b84d32c0b7bd721
YeonJuan <yeonjuan93@naver.com>
2025-09-06 20:52:15 +0900

    feat: add template info types

--- src/types/template.ts
+++ src/types/template.ts
@@ -3,6 +3,13 @@ import { Range } from "./range";
 export type TemplateInfo =
   | Range
   | {
+      type?:
+        | "inline"
+        | "blockOpen"
+        | "blockClose"
+        | "conditionalBlockOpen"
+        | "conditionalBlockClose"
+        | "alternativeBlockOpen";
       open: Range;
       close: Range;
     };

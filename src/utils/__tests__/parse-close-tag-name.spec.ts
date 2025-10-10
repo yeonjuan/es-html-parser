9bf209a7bbf2928390268981bf27dcd8469d3d35
Yeo Chen Lian <ychenlian@gmail.com>
2025-10-10 17:42:06 +0800

    Allow CRLF when parsing close tag name.

--- src/utils/__tests__/parse-close-tag-name.spec.ts
+++ src/utils/__tests__/parse-close-tag-name.spec.ts
@@ -5,6 +5,16 @@ describe("parseCloseTagName", () => {
     expect(parseCloseTagName("</div>")).toBe("div");
   });
 
+  it("Should return close tag name when close tag has LF newline before closing caret", () => {
+    const closeTag = `</div\n>`;
+    expect(parseCloseTagName(closeTag)).toBe("div");
+  });
+
+  it("Should return close tag name when close tag has CRLF newline before closing caret", () => {
+    const closeTag = `</div\r\n>`;
+    expect(parseCloseTagName(closeTag)).toBe("div");
+  });
+
   it("Should throw error", () => {
     expect(() => parseCloseTagName("<div>")).toThrowError();
   });
